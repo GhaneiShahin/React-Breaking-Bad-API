bc5e331ced09b3e81bc6dca8638759c4a98c2ca6
ShahinGhanei94 <shahinghanei94@sh94gh.local>
2020-08-04 12:47:16 +0200

    Updates

---
+++ precache-manifest.0eef6683832afcb42fbdc83607ab9875.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e9923d91de625bf75ec203fd5eb053",
+    "url": "/React-Breaking-Bad-API/index.html"
+  },
+  {
+    "revision": "352df8e0fdbfa56d9093",
+    "url": "/React-Breaking-Bad-API/static/css/main.da6dd63e.chunk.css"
+  },
+  {
+    "revision": "7de07b85c6213bb99f9e",
+    "url": "/React-Breaking-Bad-API/static/js/2.4eb6d08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Breaking-Bad-API/static/js/2.4eb6d08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352df8e0fdbfa56d9093",
+    "url": "/React-Breaking-Bad-API/static/js/main.fd37a8d0.chunk.js"
+  },
+  {
+    "revision": "9a6c282717c634c31091",
+    "url": "/React-Breaking-Bad-API/static/js/runtime-main.6c561497.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/React-Breaking-Bad-API/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/React-Breaking-Bad-API/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/React-Breaking-Bad-API/static/media/spinner.235b9b3e.gif"
+  }
+]);
